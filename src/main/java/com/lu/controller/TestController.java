e366c9b98a3414fd62475a5e4107eb126cd8cbfe
卢大哥 <913650629@qq.com>
2022-07-03 22:07:09 +0800

    数据初步已经完成

--- src/main/java/com/lu/controller/TestController.java
+++ src/main/java/com/lu/controller/TestController.java
@@ -2,28 +2,30 @@ package com.lu.controller;
 
 import com.lu.model.DemoData;
 import com.lu.model.WaitData;
-import com.lu.util.WaitToResult;
 import com.pig4cloud.plugin.excel.annotation.RequestExcel;
 import com.pig4cloud.plugin.excel.annotation.ResponseExcel;
 import com.pig4cloud.plugin.excel.annotation.Sheet;
-import com.pig4cloud.plugin.excel.vo.ErrorMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.lu.service.TestService;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
 @RequestMapping
 public class TestController {
 
-    @PostMapping("/upload")
+    @RequestMapping("/upload")
     @ResponseExcel(name = "test", sheets = @Sheet(sheetName = "testSheet1"))
-    public List<DemoData> upload(@RequestExcel List<WaitData> dataList, BindingResult bindingResult) {
+    public List<DemoData> upload(@RequestExcel List<WaitData> dataList, @RequestParam("jyhqz") String jyhqz, BindingResult bindingResult, HttpServletResponse response) {
         // JSR 303 校验通用校验获取失败的数据
 //        List<ErrorMessage> errorMessageList = (List<ErrorMessage>) bindingResult.getTarget();
-        return  WaitToResult.test(dataList);
+        //交易号前缀+所有数据
+        response.addHeader("content-Type","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        return new TestService().upload(dataList,jyhqz);
     }
 }
--- src/main/java/com/lu/model/DemoData.java
+++ src/main/java/com/lu/model/DemoData.java
@@ -6,30 +6,60 @@ import lombok.Data;
 
 @Data
 public class DemoData {
-    @ExcelIgnore
-    private WaitData waitData;
     //目标表
     //交易号	店铺名称	买家昵称	买家留言	卖家备注	收货人	收货地址	省	市	区	收货人手机	收货人电话	快递编号	快递成本	快递费用	是否货到付款	订单类型	SKU	销售单价	代理价	数量	邮编	发票抬头	发票内容	业务员	预留属性1
 
-    public DemoData(WaitData waitData) {
-        //            @ExcelProperty("买家会员名")
-//            private String mjhym;
-//            @ExcelProperty("收货人姓名")
-//            private String shrsj;
-//            @ExcelProperty("收货地址")
-//            private String shdz;
-//            @ExcelProperty("联系手机")
-//            private String lxsj;
-//            @ExcelProperty("宝贝标题")
-//            private String bbbt;
-        this.waitData = waitData;
-        this.mjnc=waitData.getMjhym();
-        this.shr=waitData.getShrxm();
-        this.shdz=waitData.getShdz();
-        this.shrsj=waitData.getLxsj();
-        this.shrdh=waitData.getLxsj();
-        this.sku=waitData.getBbbt();
 
+    public DemoData(WaitData waitData) {
+        //买家昵称、买家姓名
+        if(waitData.getMjnc()!=null&&waitData.getMjnc()!=""){
+            this.mjnc=waitData.getMjnc();
+        } else if (waitData.getMjhym()!=null||waitData.getMjhym()!="") {
+            this.mjnc=waitData.getMjhym();
+        }else {
+            this.mjnc="";
+        }
+        //买家收货人姓名，收货人
+        if(waitData.getShr()!=null&&waitData.getShr()!=""){
+            this.shr=waitData.getShr();
+        } else if (waitData.getShrxm()!=null||waitData.getShrxm()!="") {
+            this.shr=waitData.getShrxm();
+        }else {
+            this.shr="";
+        }
+
+        //收货人手机
+        if(waitData.getShrsj()!=null&&waitData.getShrsj()!=""){
+            this.shrsj=waitData.getShrsj();
+            this.shrdh=waitData.getShrsj();
+        }else if (waitData.getLxsj()!=null&&waitData.getLxsj()!=""){
+            this.shrsj=waitData.getLxsj();
+            this.shrdh=waitData.getLxsj();
+        }else if(waitData.getShrdh()!=null&&waitData.getShrdh()!=""){
+            this.shrsj=waitData.getShrdh();
+            this.shrdh=waitData.getShrdh();
+        }else {
+            this.shdz="";
+            this.shrdh="";
+        }
+        //宝贝标题、sku
+        if(waitData.getSku()!=null&&waitData.getSku()!=""){
+            this.sku=waitData.getSku();
+        }else if (waitData.getBbbt()!=null&&waitData.getBbbt()!=""){
+            this.sku=waitData.getBbbt();
+        }else {
+            this.sku="";
+        }
+
+        //收获地址、店铺名称、省、市、区
+        this.shdz=waitData.getShdz();
+        this.dpmc=waitData.getDpmc();
+        this.sheng=waitData.getSheng();
+        this.shi=waitData.getShi();
+        this.qu=waitData.getQu();
+        this.sl=waitData.getSl();
+        this.yb=waitData.getWlbz();
+        //未使用：plant、
     }
 
     @ExcelProperty("交易号")
@@ -82,8 +112,6 @@ public class DemoData {
     private String ywy;
     @ExcelProperty("预留属性1")
     private String ylsx1;
-
-
 
 
 }
