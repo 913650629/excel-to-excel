a34fe92c2c99e30026f5483c1b09beb3f66844cc
卢大哥 <913650629@qq.com>
2022-08-01 19:01:40 +0800

    初步添加对省市区的添加空格，待对自治区完善

--- src/main/java/com/lu/service/TestService.java
+++ src/main/java/com/lu/service/TestService.java
@@ -17,6 +17,23 @@ public class TestService {
                 demoDatum.setJyh(jyhqz+(demoData.indexOf(demoDatum)+1));
                 char c = chars.charAt((int)(Math.random() * 36));
                 demoDatum.setShr(demoDatum.getShr()+c);
+                //设置省市区
+                String shdz = demoDatum.getShdz();
+                shdz = shdz.replaceAll(" ", "");
+                String shdzsub = shdz.substring(0, 2);
+                if (shdzsub.contains("上海")||shdzsub.contains("北京")||shdzsub.contains("深圳")||shdzsub.contains("天津")){
+                    shdz=shdz.replaceFirst(shdzsub,shdzsub+" ");
+                }else if(shdz.contains("省")){
+                    shdz=shdz.replaceFirst("省","省 ");
+                } else if (shdz.contains("自治区")) {
+                    shdz=shdz.replaceFirst("自治区","自治区 ");
+                }
+                shdz=shdz.replaceFirst("市","市 ");
+                shdz=shdz.replaceFirst("区","区 ");
+                shdz=shdz.replaceFirst("县","县 ");
+
+                demoDatum.setShdz(shdz);
+
             }
         }
         return demoData;
